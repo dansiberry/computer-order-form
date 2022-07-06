ab93efd4f4b834bef6c321c80abffeeb5b279f47
Dan <dan@evolution7.com.au>
2022-07-06 23:48:42 +0200

    Add comments

--- cypress/e2e/products-listing.cy.js
+++ cypress/e2e/products-listing.cy.js
@@ -1,18 +1,22 @@
 describe('Products listing', () => {
     beforeEach(() => {
+        // Visit the products listing page
         cy.visit('http://localhost:3000/');
     });
 
     it('displays products on page load', () => {
+        // Test that all 6 products are showing
         cy.get('.products-grid').children().should('have.length', 6);
     });
 
     it('adds products to cart', () => {
+        // Test that adding a product to the cart updates the UI
         cy.get('.products-grid .product-card:first-child button').click();
         cy.get('.cart-count').should('contain.text', '1');
     });
 
     it('filters by product type', () => {
+        // Test that filtering by desktops only removes any laptop products
         cy.get('#product-type').select('desktops');
         cy.get('.product-card.laptops').should('not.exist');
     });
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// Import base files and create Vue.js app with router
+// Import Vue.js and base files
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/base.css';
 
+// Create the Vue.js app
 const app = createApp(App);
 
+// Use the Vue.js router
 app.use(router);
 
+// Mount the app to the HTML root element
 app.mount('#app');
